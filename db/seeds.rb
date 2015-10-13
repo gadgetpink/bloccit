eddc2d722f19a8f447aa7daace73b29b3b6f47b2
gadgetpink <diane.kulseth329@gmail.com>
2015-10-12 20:50:10 -0500

    Assignment 33 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,15 @@ end
 posts = Post.all
 
 # Create Comments
-
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
     )
 end
+
+Post.find_or_create_by(title: "Title Here", body: "Body Goes Here")
+puts "#{Post.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
